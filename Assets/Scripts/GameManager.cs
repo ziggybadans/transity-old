427b372a31fcfbbc6228a1f746afb11881dddaf5
ziggybadans <ziggyspike@hotmail.com.au>
2024-05-08 17:10:43 +1000

    Added basic UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadGameplayScene(string sceneName)
+    {
+       SceneManager.LoadScene(sceneName);
+    }
+}
